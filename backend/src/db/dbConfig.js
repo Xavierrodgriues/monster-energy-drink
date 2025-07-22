0165c7858456abee7bcf0ce0d60399149b8c029d
xavier <xavierrodgriues123@gmail.com>
2025-07-22 22:08:11 +0530

    mongodb local to mongodb atlas shifted

--- backend/src/db/dbConfig.js
+++ backend/src/db/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dbConnection = async () => {
   // MongoDB Connection
   await mongoose
-    .connect(process.env.DB_URL)
+    .connect(process.env.DB_URL || "mongodb://localhost:27017/monster_energy_drink")
     .then(() => console.log("✅ Connected to MongoDB"))
     .catch((err) => console.error("❌ MongoDB connection error:", err));
 }; 
